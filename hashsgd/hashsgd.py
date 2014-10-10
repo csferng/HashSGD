9f73a09bb107502da9822164763daf252cf9f705
csferng <ferng1021@gmail.com>
2014-10-10 20:46:41 +0800

    Add cross validation functionality and command line arguments

--- hashsgd/hashsgd.py
+++ hashsgd/hashsgd.py
@@ -18,6 +18,7 @@ as the name is changed.
 from datetime import datetime
 from math import log, exp, sqrt
 
+import argparse
 
 # TL; DR
 # the main learning process start at line 122
@@ -28,9 +29,37 @@ from math import log, exp, sqrt
 train = 'train.csv'  # path to training file
 label = 'trainLabels.csv'  # path to label file of training data
 test = 'test.csv'  # path to testing file
+prediction = 'prediction.csv'   # path to prediction file
 
 D = 2 ** 18  # number of weights use for each model, we have 32 of them
 alpha = .1   # learning rate for sgd optimization
+
+def parse_args():
+    global train, label, test, prediction, D, alpha
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-D', help='number of weights used for each model, we have 32 of them', type=int, default=2**18)
+    parser.add_argument('--alpha', '-a', help='learning rate for SGD optimization', type=float, default=.1)
+    parser.add_argument('train', help='path to training file')
+    parser.add_argument('train_label', help='path to label file of training data')
+    subparsers = parser.add_subparsers()
+    subparser_cv = subparsers.add_parser('cv', help='do cross validation')
+    subparser_cv.add_argument('--nfold', '-v', help='number of folds', type=int, default=5)
+    subparser_cv.set_defaults(cmd='cv')
+    subparser_test = subparsers.add_parser('test', help='do prediction on test data')
+    subparser_test.add_argument('test', help='path to label testing file')
+    subparser_test.add_argument('prediction', help='path to prediction file')
+    subparser_test.set_defaults(cmd='test')
+    args = parser.parse_args()
+    print args
+    train = args.train
+    label = args.train_label
+    D = args.D
+    alpha = args.alpha
+    if args.cmd == 'test':
+        test = args.test
+        prediction = args.prediction
+    return args
 
 
 # function, generator definitions ############################################
@@ -120,39 +149,92 @@ def update(alpha, w, n, x, p, y):
 
 
 # training and testing #######################################################
-start = datetime.now()
+def train_one(train, label, fold_in_cv=None):
+    # a list for range(0, 33) - 13, no need to learn y14 since it is always 0
+    K = [k for k in range(33) if k != 13]
 
-# a list for range(0, 33) - 13, no need to learn y14 since it is always 0
-K = [k for k in range(33) if k != 13]
+    # initialize our model, all 32 of them, again ignoring y14
+    w = [[0.] * D if k != 13 else None for k in range(33)]
+    n = [[0.] * D if k != 13 else None for k in range(33)]
 
-# initialize our model, all 32 of them, again ignoring y14
-w = [[0.] * D if k != 13 else None for k in range(33)]
-n = [[0.] * D if k != 13 else None for k in range(33)]
+    loss = 0.
+    loss_y14 = log(1. - 10**-15)
 
-loss = 0.
-loss_y14 = log(1. - 10**-15)
+    cnt = 0
+    for (i,(ID, x, y)) in enumerate(data(train, label)):
+        # skip validation data in training
+        if fold_in_cv is not None and i%fold_in_cv[1]==fold_in_cv[0]:
+            continue
+        cnt += 1
 
-for ID, x, y in data(train, label):
-
-    # get predictions and train on all labels
-    for k in K:
-        p = predict(x, w[k])
-        update(alpha, w[k], n[k], x, p, y[k])
-        loss += logloss(p, y[k])  # for progressive validation
-    loss += loss_y14  # the loss of y14, logloss is never zero
-
-    # print out progress, so that we know everything is working
-    if ID % 100000 == 0:
-        print('%s\tencountered: %d\tcurrent logloss: %f' % (
-            datetime.now(), ID, (loss/33.)/ID))
-
-with open('./submission1234.csv', 'w') as outfile:
-    outfile.write('id_label,pred\n')
-    for ID, x in data(test):
+        # get predictions and train on all labels
         for k in K:
             p = predict(x, w[k])
-            outfile.write('%s_y%d,%s\n' % (ID, k+1, str(p)))
-            if k == 12:
-                outfile.write('%s_y14,0.0\n' % ID)
+            update(alpha, w[k], n[k], x, p, y[k])
+            loss += logloss(p, y[k])  # for progressive validation
+        loss += loss_y14  # the loss of y14, logloss is never zero
 
-print('Done, elapsed time: %s' % str(datetime.now() - start))
+        # print out progress, so that we know everything is working
+        if cnt % 100000 == 0:
+            print('%s\ttrained now: %d\tcurrent logloss: %f' % (
+                datetime.now(), cnt, (loss/33.)/cnt))
+    print('%s\ttrained all: %d\tcurrent logloss: %f' % (
+        datetime.now(), cnt, (loss/33.)/cnt))
+
+    return w
+
+def evaluate(valid_data, label, w, fold_in_cv=None):
+    # a list for range(0, 33) - 13, no need to learn y14 since it is always 0
+    K = [k for k in range(33) if k != 13]
+
+    loss = 0.
+    loss_y14 = log(1. - 10**-15)
+
+    cnt = 0
+    for (i,(ID, x, y)) in enumerate(data(valid_data, label)):
+        # skip training data
+        if fold_in_cv is not None and i%fold_in_cv[1]!=fold_in_cv[0]:
+            continue
+        cnt += 1
+
+        # get predictions and train on all labels
+        for k in K:
+            p = predict(x, w[k])
+            loss += logloss(p, y[k])  # for progressive validation
+        loss += loss_y14  # the loss of y14, logloss is never zero
+
+    print('%s\tevaluated: %d\tcurrent logloss: %f' % (
+        datetime.now(), cnt, (loss/33.)/cnt))
+    return (cnt, loss/33.)
+
+def main():
+    start = datetime.now()
+    print('%s\tstart' % (start))
+
+    args = parse_args()
+
+    if args.cmd == 'test':   # train on training data and predict on testing data
+        w = train_one(args.train, args.train_label)
+        with open(args.prediction, 'w') as outfile:
+            outfile.write('id_label,pred\n')
+            for ID, x in data(args.test):
+                for k in K:
+                    p = predict(x, w[k])
+                    outfile.write('%s_y%d,%s\n' % (ID, k+1, str(p)))
+                    if k == 12:
+                        outfile.write('%s_y14,0.0\n' % ID)
+    else:   # do cross validation
+        nfold = args.nfold
+        cnt_ins = 0
+        cnt_loss = 0.
+        for fold in xrange(nfold):
+            w = train_one(args.train, args.train_label, (fold,nfold))
+            f_ins, f_loss = evaluate(args.train, args.train_label, w, (fold,nfold))
+            cnt_ins += f_ins
+            cnt_loss += f_loss
+        print "CV result: %f"%(cnt_loss/cnt_ins)
+
+    print('Done, elapsed time: %s' % str(datetime.now() - start))
+
+if __name__== '__main__':
+    main()
